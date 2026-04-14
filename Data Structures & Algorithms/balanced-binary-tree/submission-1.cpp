acaeb6943f73a5ca2fac23e94196210ffd6f2fd5
Sumit Shah <sumit8692@gmail.com>
2026-04-14 11:34:01 -0400

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+    int height(TreeNode* root){
+        if(root == NULL)    return 0;
+        
+        int left = height(root->left);
+        int right = height(root->right);
+
+        return max(left, right) + 1;
+
+    }
+    bool isBalanced(TreeNode* root) {
+
+        if(root == NULL)    return true;
+
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+
+        if(abs(leftHeight - rightHeight) > 1)    return false;
+        return isBalanced(root->left) && isBalanced(root->right);
+
+    }
+};
